a1031062d5bed5b59ba0bd96c73a0d994943b5e7
BenjaminCIR <90195757+BenjaminCIR@users.noreply.github.com>
2024-05-21 17:10:14 +0200

    front-web init

--- linx/src/App.js
+++ linx/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from '../public/Ad';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="./AddicteALaLigue512.png" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br></br>
+          Linx.exe ^^
         </p>
         <a
           className="App-link"
