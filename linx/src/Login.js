f907a03e30e469069805e1decc567bbad67529a1
amine777777 <amine.elbaks@gmail.com>
2024-05-22 17:52:24 +0200

    yassine message

--- linx/src/Login.js
+++ linx/src/Login.js
@@ -1,3 +1,26 @@
+// import React from 'react';
+// import './Login.css';
+
+// function Login() {
+//   return (
+//     <div className="login-container">
+//       <div className="login-form">
+//         <h1>Login</h1>
+//         <form>
+//           <label>Email:</label>
+//           <input type="email" name="email" />
+//           <label>Password:</label>
+//           <input type="password" name="password" />
+//           <button type="submit">Login</button>
+//         </form>
+//         <a href="/signup">Don't have an account? Sign Up</a>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
 import React from 'react';
 import './Login.css';
 
@@ -5,15 +28,19 @@ function Login() {
   return (
     <div className="login-container">
       <div className="login-form">
-        <h1>Login</h1>
-        <form>
-          <label>Email:</label>
-          <input type="email" name="email" />
-          <label>Password:</label>
-          <input type="password" name="password" />
-          <button type="submit">Login</button>
+        <h1 className="login-title">Login</h1>
+        <form onSubmit={(e) => e.preventDefault()}>  {/* Prevent default form submission */}
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" id="email" required />
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password" id="password" required />
+          <button type="submit" className="login-btn">
+            Login
+          </button>
         </form>
-        <a href="/signup">Don't have an account? Sign Up</a>
+        <a href="/signup" className="signup-link">
+          Don't have an account? Sign Up
+        </a>
       </div>
     </div>
   );
