a99a63db8b01bdd9f749e94ea0e348101cdd6cb7
BenjaminCIR <90195757+BenjaminCIR@users.noreply.github.com>
2024-05-22 11:56:01 +0200

    Home

--- linx/src/App.js
+++ linx/src/App.js
@@ -1,26 +1,20 @@
 //import logo from '../public/Ad';
 import './css/App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="./AddicteALaLigue512.png" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br></br>
-          Linx.exe ^^
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Home />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </Router>
   );
 }
 
